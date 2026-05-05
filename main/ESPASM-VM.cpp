eaa2764fae3fa932b0b28614c0705472ae9589bd
12354fdw <88530369+12354fdw@users.noreply.github.com>
2026-05-05 15:57:32 +0700

    supressed warnings

--- main/ESPASM-VM.cpp
+++ main/ESPASM-VM.cpp
@@ -1,23 +1,7 @@
-#include "esp_err.h"
 #include "esp_log.h"
-#include "nvs.h"
-#include "nvs_flash.h"
-
-static const char* TAG = "ESPASM";
+#include "global.hpp"
 
 extern "C" void app_main(void) {
-
-	// init non volatile storage
-	nvs_flash_init_label:
-	esp_err_t nvs_ret = nvs_flash_init();
-	if (nvs_ret == ESP_ERR_NVS_NO_FREE_PAGES ||
-		nvs_ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-		
-		ESP_LOGE(TAG, "NVS init error!");
-		ESP_ERROR_CHECK(nvs_flash_erase());
-		goto nvs_flash_init_label;
-	}
-	ESP_LOGI(TAG, "initalized NVS!");
 
 	ESP_LOGI(TAG, "Hello, world!");
 }
