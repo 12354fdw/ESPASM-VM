9479e353298de429d2951afbcea33365ed25d527
12354fdw <88530369+12354fdw@users.noreply.github.com>
2026-05-06 11:24:32 +0700

    fixed clangd complaining

---
+++ components/init_systems/init.cpp
@@ -0,0 +1,39 @@
+#include "esp_event.h"
+#include "esp_log.h"
+#include "esp_netif.h"
+#include "includes/init.hpp"
+#include "nvs_flash.h"
+
+static const char *TAG = "INIT";
+
+static void init_nvs() {
+	esp_err_t ret = nvs_flash_init();
+
+	if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
+		ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+		ESP_LOGW(TAG, "BAD NVS, ERASING");
+		ESP_ERROR_CHECK(nvs_flash_erase());
+		ret = nvs_flash_init();
+	}
+
+	ESP_ERROR_CHECK(ret);
+	ESP_LOGI(TAG, "[  OK  ] NVS");
+}
+
+static void init_netif() {
+	ESP_ERROR_CHECK(esp_netif_init());
+	ESP_ERROR_CHECK(esp_event_loop_create_default());
+	ESP_LOGI(TAG, "[  OK  ] netif");
+}
+
+void init_systems() {
+	init_nvs();
+	init_netif();
+
+	ESP_LOGI(TAG, "ESP32 INITALIZED");
+
+	ESP_LOGI("MEMCHECK", "FREE HEAP: %lu bytes",
+			 heap_caps_get_free_size(MALLOC_CAP_8BIT));
+	ESP_LOGI("MEMCHECK", "LARGEST BLOCK: %lu bytes",
+			 heap_caps_get_largest_free_block(MALLOC_CAP_8BIT));
+}
--- main/ESPASM-VM.cpp
+++ main/ESPASM-VM.cpp
@@ -1,7 +1,9 @@
 #include "esp_log.h"
 #include "global.hpp"
+#include "../components/init_systems/includes/init.hpp"
 
 extern "C" void app_main(void) {
+	init_systems();
 
 	ESP_LOGI(TAG, "Hello, world!");
 }
